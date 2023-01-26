81f08e6d3bb0479d0d79ca6e8a453b130c79dc67
8delphi9 <8delphi9@naver.com>
2023-01-26 22:24:11 +0900

    Feature: Banner, Service 영역 이미지 삽입

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Banners
+from .models import Banners, Service
 
-admin.site.register(Banners)
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('alt_text', 'image_tag')
+
+
+admin.site.register(Banners, BannerAdmin)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image_tag')
+
+
+admin.site.register(Service, ServiceAdmin)
--- main/models.py
+++ main/models.py
@@ -1,16 +1,28 @@
 from django.db import models
-
+from django.utils.html import mark_safe
 
 
 class Banners(models.Model):
-    img=models.ImageField(upload_to="banners/")
+    img = models.ImageField(upload_to="banners/")
     alt_text = models.CharField(max_length=150)
+
+    class Meta:
+        verbose_name_plural = "1. Banners"
 
     def __str__(self):
         return self.alt_text
+
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="70" />' % self.img.url)
 
 
 class Service(models.Model):
     title = models.CharField(max_length=150)
     detail = models.TextField()
+    img = models.ImageField(upload_to='services/', null=True)
 
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="%s" width="70" />' % self.img.url)
