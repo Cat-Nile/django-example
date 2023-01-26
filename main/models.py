d3fd7e5f74eaa82262b853c287682024746938f7
8delphi9 <8delphi9@naver.com>
2023-01-26 21:45:43 +0900

    Feature: Media 경로 설정

--- main/models.py
+++ main/models.py
@@ -6,6 +6,10 @@ class Banners(models.Model):
     img=models.ImageField(upload_to="banners/")
     alt_text = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.alt_text
+
+
 class Service(models.Model):
     title = models.CharField(max_length=150)
     detail = models.TextField()
